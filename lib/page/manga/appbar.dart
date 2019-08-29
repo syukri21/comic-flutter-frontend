71502c92c7aa47de13431b3827b519cf7bfc9eea
Syukri <syukrihsb148@gmail.com>
2019-08-30 03:12:47 +0700

    fix appbar

--- lib/page/manga/appbar.dart
+++ lib/page/manga/appbar.dart
@@ -3,69 +3,110 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 List<Widget> mangaAppbar(bool innerBoxIsScrolled, comic) {
+  const double expandedHeight = 300;
+
   return <Widget>[
     SliverAppBar(
+      expandedHeight: expandedHeight,
       pinned: true,
-      forceElevated: innerBoxIsScrolled,
-      floating: true,
-      title: Text("Bangsat"),
+      automaticallyImplyLeading: true,
+      flexibleSpace: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return MangaAppbarFlexible(
+            comic: comic,
+            height: constraints.biggest.height,
+          );
+        },
+      ),
     ),
-    SliverAppBar(
-      expandedHeight: 300,
-      pinned: false,
-      automaticallyImplyLeading: false,
-      flexibleSpace: Stack(
-        children: <Widget>[
-          Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: FlexibleSpaceBar(
-                  background: CachedNetworkImage(
-                    imageUrl: comic["image"],
-                    imageBuilder: (BuildContext context, provider) => Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: provider,
-                          fit: BoxFit.cover,
-                        ),
+  ];
+}
+
+class MangaAppbarFlexible extends StatelessWidget {
+  final comic;
+  final double height;
+
+  MangaAppbarFlexible({
+    Key key,
+    this.comic,
+    this.height,
+  }) : super(key: key);
+
+  int get bottomBackgroundFlex {
+    int height = (this.height.toInt() - 200) ~/ 4;
+    if (height > 0) return height;
+    return 0;
+  }
+
+  double get opacity {
+    int flex = bottomBackgroundFlex - 2;
+    if (flex > 31) {
+      flex = 31;
+    } else if (flex < 1) {
+      flex = 0;
+    }
+
+    return flex / 31;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 60,
+              child: FlexibleSpaceBar(
+                background: CachedNetworkImage(
+                  imageUrl: comic["image"],
+                  imageBuilder: (BuildContext context, provider) => Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: provider,
+                        fit: BoxFit.cover,
                       ),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: <Color>[
-                              Theme.of(context).primaryColor,
-                              Colors.transparent,
-                            ],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                          ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Theme.of(context).primaryColor,
+                            Colors.transparent,
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  color: Color(0xffFAFAFA),
-                ),
-              ),
-            ],
-          ),
-          Center(
-            child: Align(
-              alignment: Alignment.bottomCenter,
+            ),
+            Expanded(
+              flex: bottomBackgroundFlex,
               child: Container(
-                margin: const EdgeInsets.only(top: 70),
+                color: Color(0xffFAFAFA),
+              ),
+            ),
+          ],
+        ),
+        Center(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Opacity(
+              opacity: opacity,
+              child: Container(
                 width: 150,
                 padding: const EdgeInsets.all(8),
                 decoration: BoxDecoration(
                   color: Color(0xffFAFAFA),
-                  borderRadius: BorderRadius.circular(4),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(4),
+                    topRight: Radius.circular(4),
+                  ),
                 ),
                 child: CachedNetworkImage(
                   imageUrl: comic["image"],
@@ -74,9 +115,9 @@ List<Widget> mangaAppbar(bool innerBoxIsScrolled, comic) {
                 ),
               ),
             ),
-          )
-        ],
-      ),
-    ),
-  ];
+          ),
+        )
+      ],
+    );
+  }
 }
