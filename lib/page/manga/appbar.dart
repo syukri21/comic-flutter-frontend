6a1f2fa23fce46013d73f8aedd0f0da2049671f7
Syukri <syukrihsb148@gmail.com>
2019-08-30 16:32:53 +0700

    change structure on manga appbar

--- lib/page/manga/appbar.dart
+++ lib/page/manga/appbar.dart
@@ -51,70 +51,105 @@ class MangaAppbarFlexible extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              flex: 93,
-              child: FlexibleSpaceBar(
-                background: CachedNetworkImage(
-                  imageUrl: comic["image"],
-                  imageBuilder: (BuildContext context, provider) => Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: provider,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: <Color>[
-                            Theme.of(context).primaryColor,
-                            Colors.transparent,
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: bottomBackgroundFlex,
-              child: Container(
-                color: Color(0xffFAFAFA),
-              ),
-            ),
-          ],
+        BackgroundAppbar(
+          comic: comic,
+          bottomBackgroundFlex: bottomBackgroundFlex,
         ),
-        Center(
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Opacity(
-              opacity: opacity,
-              child: Container(
-                width: 150,
-                padding: const EdgeInsets.all(8),
+        CenterAppbar(opacity: opacity, comic: comic)
+      ],
+    );
+  }
+}
+
+class CenterAppbar extends StatelessWidget {
+  const CenterAppbar({
+    Key key,
+    @required this.opacity,
+    @required this.comic,
+  }) : super(key: key);
+
+  final double opacity;
+  final comic;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Opacity(
+          opacity: opacity,
+          child: Container(
+            width: 150,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Color(0xffFAFAFA),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(4),
+                topRight: Radius.circular(4),
+              ),
+            ),
+            child: CachedNetworkImage(
+              imageUrl: comic["image"],
+              fit: BoxFit.cover,
+              width: 150,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BackgroundAppbar extends StatelessWidget {
+  const BackgroundAppbar({
+    Key key,
+    @required this.comic,
+    @required this.bottomBackgroundFlex,
+  }) : super(key: key);
+
+  final comic;
+  final int bottomBackgroundFlex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 93,
+          child: FlexibleSpaceBar(
+            background: CachedNetworkImage(
+              imageUrl: comic["image"],
+              imageBuilder: (BuildContext context, provider) => Container(
                 decoration: BoxDecoration(
-                  color: Color(0xffFAFAFA),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(4),
-                    topRight: Radius.circular(4),
+                  image: DecorationImage(
+                    image: provider,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                child: CachedNetworkImage(
-                  imageUrl: comic["image"],
-                  fit: BoxFit.cover,
-                  width: 150,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Theme.of(context).primaryColor,
+                        Colors.transparent,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-        )
+        ),
+        Expanded(
+          flex: bottomBackgroundFlex,
+          child: Container(
+            color: Color(0xffFAFAFA),
+          ),
+        ),
       ],
     );
   }
